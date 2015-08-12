4887a3f3510ee8c6ddf0cdb5c445f6f00006df50
PauloCh <paulo@wavec.org>
2015-08-12 16:02:40 +0100

    WP2 inputs loaded

--- src/wp5/load/wpBoM.py
+++ src/wp5/load/wpBoM.py
@@ -7,6 +7,20 @@ Created on Wed Aug 12 15:26:10 2015
 
 import pandas as pd
 
+def load_WP2_BoM(file_path):
+    
+    excel = pd.ExcelFile(file_path)
+    # Collect data from a particular tab
+    units = excel.parse('units', header=0, index_col=0)
+    position = excel.parse('position', header=0, index_col=0)
+
+    WP2_BoM = {'numUnits': units,
+               'position':  position
+               }
+    
+    return WP2_BoM
+    
+    
 def load_WP4_BoM(file_path):
     
     # Transform vessel database .xls into panda type
