3ea6d7af93e1277dd55e385d9cc66c44eb109b52
PauloCh <paulo@wavec.org>
2015-08-12 16:27:50 +0100

    finalized the draft list of WPs inputs

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ from os import path
 import pandas as pd
 
 from wp5.load import load_vessel_data, load_equipment_data, load_port_data
-from wp5.load.wpBoM import load_WP2_BoM, load_WP4_BoM
+from wp5.load.wpBoM import load_WP1_BoM, load_WP2_BoM, load_WP3_BoM, load_WP4_BoM
 from wp5.logistics.operations import logOp_init
 from wp5.logistics.phase import logPhase_init
 
@@ -27,5 +27,10 @@ logOp = logOp_init()
 logPhase_install = logPhase_init(logOp, vessels, equipments)[0]
 logPhase_OM = logPhase_init(logOp, vessels, equipments)[1]
 
+WP1_BoM = load_WP1_BoM(database_file("WP1_BoM.xlsx"),database_file("VianaCastelo.csv"))
 WP2_BoM = load_WP2_BoM(database_file("WP2_BoM.xlsx"))
+WP3_BoM = load_WP3_BoM(database_file("WP3_BoM.xlsx"))
 WP4_BoM = load_WP4_BoM(database_file("WP4_BoM.csv"))
+
+
+
--- src/wp5/load/wpBoM.py
+++ src/wp5/load/wpBoM.py
@@ -7,6 +7,19 @@ Created on Wed Aug 12 15:26:10 2015
 
 import pandas as pd
 
+def load_WP1_BoM(file_path_device, file_path_metocean):
+    
+    excel = pd.ExcelFile(file_path_device)
+    metocean = pd.read_csv(file_path_metocean)
+    # Collect data from a particular tab
+    device = excel.parse('device', header=0, index_col=0)
+
+    WP1_BoM = {'device': device,
+               'metocean':  metocean
+               }
+    
+    return WP1_BoM
+
 def load_WP2_BoM(file_path):
     
     excel = pd.ExcelFile(file_path)
@@ -20,6 +33,16 @@ def load_WP2_BoM(file_path):
     
     return WP2_BoM
     
+def load_WP3_BoM(file_path):
+    
+    excel = pd.ExcelFile(file_path)
+    # Collect data from a particular tab
+    layout = excel.parse('Sheet1', header=0, index_col=0)
+
+    WP3_BoM = {'layout': layout,
+               }
+    
+    return WP3_BoM
     
 def load_WP4_BoM(file_path):
     
