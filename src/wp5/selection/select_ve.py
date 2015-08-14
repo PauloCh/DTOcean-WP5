c2f6e4875b9678ef9a39fa5579fe7b215d6b36ac
Boris Teillant <boris.teillant@wavec.org>
2015-08-14 17:39:21 +0100

    ongoing selec 2

--- src/wp5/selection/select_ve.py
+++ src/wp5/selection/select_ve.py
@@ -11,9 +11,13 @@ def select_e (install, log_phase):
     req_e = install['requirement'][0]
     for typ in range(len(req_e)):
         if req_e[req_e.keys()[typ]][1] == 'sup':
-            for 
-            log_phase.op_ve[1].ve_combination[1]['vessel'][1][1].id
-                
+            for seq in range(len(log_phase.op_ve)):
+                for combi in range(len(log_phase.op_ve[seq].ve_combination)):
+                    if log_phase.op_ve[seq].ve_combination[combi]['equipment'][typ][1].id == req_e.keys()[typ]:
+                        eq_pa = req_e[req_e.keys()[typ]][2]
+
+                        pd_vessel[pd_vessel['Vessel Type'] >= eq_pa
+]
                 
 def select_v (install, log_phase_id):
     
