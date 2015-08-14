e6f1a3d2535e81142c6adef9248536670e3e67b1
Boris Teillant <boris.teillant@wavec.org>
2015-08-14 14:38:46 +0100

    wp4_feas and selec_ve created

--- src/wp5/feasibility/wp4_feas.py
+++ src/wp5/feasibility/wp4_feas.py
@@ -6,11 +6,11 @@ Created on Fri Aug 14 11:51:31 2015
 """
 
 def wp4_feas(install, log_phase_id, wp2_outputs, wp4_outputs):
+
     if log_phase_id == 'F_driven':
-        for seq in log_phase.op_ve:
-            log_phase.op_ve[1].ve_combination[1]['equipment'][1].id
-        
-        if log_phase
+       # Equipment feasiblity
+       # Hammer sleeve diameter
+        F_diam = []
         load_u_f = [] # list of loading due to each foundation per unit
         area_u_f = [] # list of area occupied by each foundation per unit
         load_u = [] # list of loading due to the set of foundation(s) per unit
@@ -22,16 +22,27 @@ def wp4_feas(install, log_phase_id, wp2_outputs, wp4_outputs):
                 key3 = "weight foundation " + str(x) + " [kg]"
                 load_u_f[len(load_u_f):] = [wp4_outputs[key1].ix[0]*wp4_outputs[key2].ix[0]/wp4_outputs[key3].ix[0]]
                 area_u_f[len(area_u_f):] = [wp4_outputs[key1].ix[0]*wp4_outputs[key2].ix[0]]
+                F_diam[len(F_diam):] = wp4_outputs[key1].ix[0]
             load_u[len(load_u):] = max(load_u_f[dev*x:(dev+1)*x])
             area_u[len(area_u):] = sum(area_u_f[dev*x:(dev+1)*x])
         deck_loading = max(load_u)
         deck_area = max(area_u)
+        sleeve_diam = max(F_diam)
+        feasibility = {'equipment': {'Hammer': ['Sleeve diameter', 'sup', sleeve_diam]},
+                        'vessel': {'Crane Barge': [['Deck loading', 'sup', deck_loading],
+                                                   ['Deck area', 'sup', deck_area]],
+                                   'Crane Vessel': [['Deck loading', 'sup', deck_loading],
+                                                   ['Deck area', 'sup', deck_area]],
+                                   'JUP barge': [['Deck loading', 'sup', deck_loading],
+                                                   ['Deck area', 'sup', deck_area]],
+                                   'JUP vessel': [['Deck loading', 'sup', deck_loading],
+                                                   ['Deck area', 'sup', deck_area]]}}
     elif log_phase_id == 'F_suction':
         # TO-DO
     elif log_phase_id == 'F_gravity':
         # TO-DO
     
     feasibility = {'equipment': [],
-                   'vessel': {'Deck loading [m2/ton]': ['Sup', 'all', deck_loading],
-                                  'Deck area [m2]': ['Sup', 'all', deck_area]}}
+                   'vessel': {'Crane Barge': ['Deck loading [m2/ton]','Sup', deck_loading],
+                              ]}}
     return feasibility
---
+++ src/wp5/selection/select_ve.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 14 12:51:10 2015
+
+@author: BTeillant
+"""
+def select_ve (install, log_phase_id, vessels, equipments):
+#    for seq in range(len(log_phase.op_ve)):
+#            if log_phase.op_ve[seq].ve_combination[1]['equipment'][1].id == 'Hammer':
+elec
