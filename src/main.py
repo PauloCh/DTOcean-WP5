75c16935e60ac866f983fa24f1dc17939631dd29
pedro <pedrocivente@gmail.com>
2015-11-11 17:58:05 +0000

    testing

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@
 @author: WavEC Offshore Renewables
 email: boris.teillant@wavec.org; paulo@wavec.org
 
+
+
 main.py is the main file of the WP5 module within the suite of design tools
 developped under the EU FP7 DTOcean project. main.py provides an estimation of
 the predicted performance of feasible maritime infrastructure solutions
