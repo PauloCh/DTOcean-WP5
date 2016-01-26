ad634f0e55b1d0fbbda1b2d6a60b5d8ec1b24d1b
pedro <pedrocivente@gmail.com>
2016-01-26 16:35:16 +0000

    cleaning conflicts

--- src/Logistics/performance/schedule/schedule.py
+++ src/Logistics/performance/schedule/schedule.py
@@ -121,15 +121,10 @@ def sched(x, install, log_phase, log_phase_id,
                         elem_area = [0]*nb_dev
                         elem_mass = [0]*nb_dev
                         for dev in range(nb_dev): # loop over the number of devices
-                            # calculate the area and dry mass of all elements
-<<<<<<< HEAD
-                            elem_area[dev] = user_inputs['device']['length [m]'].ix[0]*user_inputs['device']['width [m]'].ix[0]
-                            elem_mass[dev] = user_inputs['device']['dry mass [kg]'].ix[0]/1000
-=======
+                            # calculate the area and dry mass of all elements=======
                             elem_area[dev] = user_inputs['device']['length [m]'].ix[0]*user_inputs['device']['width [m]'].ix[0] # [m^2]
                             elem_mass[dev] = user_inputs['device']['dry mass [kg]'].ix[0]/1000 # [ton]
                             
->>>>>>> 589bf62a3545f9674767c94075da855a0179e66d
                         nb_elem_port = nb_dev # initialise the number of elements to be transported that are initially at port
                         nb_journey = 0 # initialise the number of vessel journeys
                         nb_el_journey = [] # initialise the list of number of elements per journey
